568aadd87ac95e92e1ed86ef29174778ae68fb30
Aleex87 <alessandrodanteabbate@gmail.com>
2026-02-16 14:38:51 +0100

    main done, run first time the project now add a code for evalutation and save the model that perform best

--- main.py
+++ main.py
@@ -1,11 +1,39 @@
+import torch
+import torch.nn as nn
+
 from src.dataset import get_dataloaders
+from src.model import SimpleCNN
+from src.train import train_model
 
 def main():
+# === Device ===
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("using device:", device)
+
+# === Data ===
     train_loader, val_loader, test_loader = get_dataloaders(batch_size=32)
 
-    images, labels = next(iter(train_loader))
-    print(images.shape)   # expected: torch.Size([32, 3, 32, 32])
-    print(labels.shape)   # expected: torch.Size([32])
+# === Model ===
+    model = SimpleCNN(num_classes=10)
+
+# === Loss Function ===
+    criterion= nn.CrossEntropyLoss()
+
+# === Optimizer ===
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+# === Train ===
+    train_model(
+        model=model,
+        train_loader=train_loader,
+        val_loader=val_loader,
+        criterion=criterion,
+        optimizer=optimizer,
+        device=device,
+        num_epochs=5
+    )
+
+
 
 if __name__ == "__main__":
     main()
